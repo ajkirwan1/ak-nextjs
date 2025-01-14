0a7b1871c4dd257d68df0d571a62ee8dc86f0831
Adam Kirwan <ajkirwan1@gmail.com>
2025-01-14 08:11:17 +0000

    commit

--- server/actions/contact-us-action.js
+++ server/actions/contact-us-action.js
@@ -2,26 +2,37 @@
 "use server";
 
 import { sendMail } from "@/lib/send-mail";
+import ValidateContactForm from "@/lib/validation/contact-form-validate";
 
 export async function ContactUs(_, formData) {
+  let errors = [];
 
-    // const message = formData.get("message");
+  try {
     const name = formData.get("name");
     const surname = formData.get("surname");
     const email = formData.get("email");
+    const message = formData.get("message");
 
+    const validateResult = ValidateContactForm(name, surname, email, message);
 
-    // console.log(message);
-    console.log(name);
-    console.log(surname);
-    console.log(email);
+    if (!validateResult.success) {
+      // console.log(validateResult.error.issues)
 
+      for (var i = 0; i < validateResult.error.issues.length; i++) {
+        // errArr.push({ for: err[i].path[0], message: err[i].message });
+        console.log(validateResult.error.issues[i].message, "JDAJKDJDAJKD")
+      }
 
-      // const response = await sendMail({
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  // const response = await sendMail({
   //   email: "ajkirwan1gmail.com",
   //   subject: "A test email",
   //   message: "Hello Jimmy",
   //   text: message,
   // });
-
 }
