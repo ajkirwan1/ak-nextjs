65ea5eaa18fbccf4990329822a3edaf9f758c14e
Adam Kirwan <ajkirwan1@gmail.com>
2025-01-15 20:23:35 +0000

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  publicRuntimeConfig: {
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://yourdomain.com",
+  },
+};
 
 export default nextConfig;
