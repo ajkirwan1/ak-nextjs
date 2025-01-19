1e27978c5b2caa256b1cbd0087aaaa71b89dc8c6
KasiaRaven26 <rejven777@gmail.com>
2025-01-19 17:05:13 +0200

    commitedonsunday

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
 import logo from "@/public/a-k-logo.png";
+import { motion } from "motion/react";
+import { AnimatePresence } from "motion/react";
 
 export default function Home() {
   // throw Error
-  console.log(process.env.REACT_APP_BASE_URL)
+  console.log(process.env.REACT_APP_BASE_URL);
   return (
     // <div className={styles.page}>
 
@@ -25,19 +28,29 @@ export default function Home() {
           src={logo}
         ></Image>
       </div>
-      <div className={styles.menu}>
-        <div className={styles.menuItem}>
-          <Link href="/who-we-are">WHO WE ARE</Link>
-          <div className={styles.slash}></div>
-        </div>
-        <div className={styles.menuItem}>
-          <Link href="/work">WORK</Link>
-          <div className={styles.slash}></div>
-        </div>
-        <div className={styles.menuItem}>
-          <Link href="/contact">CONTACT</Link>
-        </div>
-      </div>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          exit={{ opacity: 0 }}
+          transition={{
+            type: "easeInOut",
+            duration: 2,
+          }}
+          className={styles.menu}
+        >
+          <div className={styles.menuItem}>
+            <Link href="/who-we-are">WHO WE ARE</Link>
+            <div className={styles.slash}></div>
+          </div>
+          <div className={styles.menuItem}>
+            <Link href="/work">WORK</Link>
+            <div className={styles.slash}></div>
+          </div>
+          <div className={styles.menuItem}>
+            <Link href="/contact">CONTACT</Link>
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
