22208fcdf156d0fa463611cda0450aa85a547a8a
Adam Kirwan <ajkirwan1@gmail.com>
2025-07-04 16:09:29 +0100

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
+    publicRuntimeConfig: {
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://yourdomain.com",
   },
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
